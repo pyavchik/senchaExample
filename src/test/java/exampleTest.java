8399414460815bc6afbf4c3f22acfe8e32fb23a7
pyavchik <pyavchik@gmail.com>
2018-09-24 12:33:37 +0300

    added example trueautamation + http://examples.sencha.com

---
+++ src/test/java/exampleTest.java
@@ -0,0 +1,42 @@
+import io.trueautomation.client.driver.TrueAutomationDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static io.trueautomation.client.TrueAutomationHelper.ta;
+
+public class exampleTest {
+    private WebDriver driver;
+    private By loginBtn = By.xpath(ta("loginBtn", ".//button[@id='ext-element-43']"));
+    private By dropdownAllmanagers = By.xpath(ta("dropdownAllmanagers", ".//*[@id='ext-element-144']"));
+    private By firstManagerName = By.cssSelector(ta("dropdownAllmanagers", ".x-list-label"));
+
+
+    @BeforeTest
+    public void beforeTest() {
+        driver = new TrueAutomationDriver();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void exampleTest() throws InterruptedException {
+        driver.get("http://examples.sencha.com/coworkee/#login");
+        Thread.sleep(3000);
+        driver.findElement(loginBtn).click();
+        driver.get("http://examples.sencha.com/coworkee/#organizations");
+        Thread.sleep(3000);
+        driver.findElement(dropdownAllmanagers).click();
+        Thread.sleep(3000);
+        driver.findElement(firstManagerName).click();
+        Thread.sleep(30000);
+    }
+
+    @AfterTest
+    public void afterTest() {
+        driver.quit();
+    }
+}
